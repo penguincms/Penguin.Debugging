90f0b9a66a4ef812f5b2034d3f3af463724884b1
MrJackSpade <loffredo.ap@gmail.com>
2020-02-18 12:30:22 -0700

    Auto Commit

--- GlobalSuppressions.cs
+++ GlobalSuppressions.cs
@@ -1,8 +1,6 @@
-﻿
-// This file is used by Code Analysis to maintain SuppressMessage 
+﻿// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
+// Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:Penguin.Debugging.StaticLogger.Log(System.Exception,Penguin.Debugging.StaticLogger.LoggingLevel)")]
-
--- StaticLogger.cs
+++ StaticLogger.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using Penguin.Extensions.Exceptions;
+using System;
 using System.Text;
-using Penguin.Extensions.Exceptions;
 
 namespace Penguin.Debugging
 {
@@ -11,6 +11,34 @@ namespace Penguin.Debugging
     /// </summary>
     public static class StaticLogger
     {
+        private static object ObjectLock = new object();
+
+        private static StringBuilder Queue = new StringBuilder();
+
+        /// <summary>
+        /// This func should accept the string to be logged, and return whether or not the log was successful.
+        /// If it returns false, the message queue will not be cleared. This is incase the message is logged before
+        /// your output is ready to handle the message. The message is returned as a single new-line seperated string
+        /// for speed
+        /// </summary>
+        public static Func<string, bool> Endpoint { get; set; }
+
+        /// <summary>
+        /// If this bool is false, theres no point in logging anything since its not going anywhere
+        /// </summary>
+        public static bool IsListening
+        {
+            get
+            {
+                return Level != LoggingLevel.None;
+            }
+        }
+
+        /// <summary>
+        /// This should represent how often messages are flushed to the endpoint
+        /// </summary>
+        public static LoggingLevel Level { get; set; } = LoggingLevel.None;
+
         /// <summary>
         /// Represents the frequency that messages will be send to the logging endpoint
         /// </summary>
@@ -40,34 +68,7 @@ namespace Penguin.Debugging
             /// Flushed messages should only occur on finalization of a process, for when a process is succeeding but its state is not correct
             /// </summary>
             Final = 4,
-
-
         }
-
-        /// <summary>
-        /// This func should accept the string to be logged, and return whether or not the log was successful.
-        /// If it returns false, the message queue will not be cleared. This is incase the message is logged before
-        /// your output is ready to handle the message. The message is returned as a single new-line seperated string
-        /// for speed
-        /// </summary>
-        public static Func<string, bool> Endpoint { get; set; }
-
-
-        /// <summary>
-        /// If this bool is false, theres no point in logging anything since its not going anywhere
-        /// </summary>
-        public static bool IsListening
-        {
-            get
-            {
-                return Level != LoggingLevel.None;
-            }
-        }
-
-        /// <summary>
-        /// This should represent how often messages are flushed to the endpoint
-        /// </summary>
-        public static LoggingLevel Level { get; set; } = LoggingLevel.None;
 
         /// <summary>
         /// Attempts to flush the message queue to the endpoint. returns false if the endpoint reports a failure
@@ -126,13 +127,14 @@ namespace Penguin.Debugging
         /// <param name="levelToLog">Allows the level to be overridden</param>
         public static void Log(Exception ex, LoggingLevel levelToLog = LoggingLevel.Exception)
         {
-            if(IsListening)
+            if (IsListening)
             {
                 try
                 {
                     Log(ex.RecursiveMessage(), levelToLog);
                     Log(ex.RecursiveStackTrace(), levelToLog);
-                } catch (Exception iex)
+                }
+                catch (Exception iex)
                 {
                     Log("An exception has been encountered while logging the previous exception", LoggingLevel.Exception);
                     Log(iex.Message, LoggingLevel.Exception);
@@ -140,8 +142,5 @@ namespace Penguin.Debugging
                 }
             }
         }
-
-        private static object ObjectLock = new object();
-        private static StringBuilder Queue = new StringBuilder();
     }
 }
